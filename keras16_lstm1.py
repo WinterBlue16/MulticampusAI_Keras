ff8a7c0f2aa686fa8e7dae3e9970f502fbeeecb3
Winter Lee <leekh090163@gmail.com>
2020-01-30 13:00:50 +0900

    file revised

--- keras16_lstm1.py
+++ keras16_lstm1.py
@@ -29,23 +29,24 @@ model = Sequential()
 
 # 해결방법 1. LSTM의 파라미터 return_sequences 이용(False => True)
 model.add(LSTM(128, activation='relu', input_shape=(3, 1), return_sequences=True))
+
 # 해결방법 2. keras.layers.Reshape 이용(Layer 추가 => model.add(Reshape((전 layer node 수, input_shape와 일치)))
 # model.add(Reshape((128, 1)))
 model.add(LSTM(64, activation='relu', return_sequences=True))
 model.add(LSTM(256, activation='selu', return_sequences=True))
-model.add(LSTM(256, activation='selu', return_sequences=True))
-model.add(LSTM(8, activation='selu', return_sequences=True))
-model.add(LSTM(256, activation='elu', return_sequences=True))
-model.add(LSTM(128, activation='selu', return_sequences=True))
-model.add(LSTM(256, activation='selu', return_sequences=True))
-model.add(LSTM(64, activation='selu', return_sequences=True))
-model.add(LSTM(8, activation='selu'))
+model.add(LSTM(256, activation='selu'))
+# model.add(LSTM(8, activation='selu', return_sequences=True))
+# model.add(LSTM(256, activation='elu', return_sequences=True))
+# model.add(LSTM(128, activation='selu', return_sequences=True))
+# model.add(LSTM(256, activation='selu', return_sequences=True))
+# model.add(LSTM(64, activation='selu', return_sequences=True))
+# model.add(LSTM(4, activation='selu')) 
+# 지나치게 많은 LSTM Layer를 쌓을 경우 과적합 혹은 시계열 데이터에서 벗어나 정확한 예측이 안 될 가능성이 높다.
 model.add(Dense(1))
-
 
 model.summary()
 
-# 5. 모델 훈련시키기(EarlyStopping 추가)
+# 4. 모델 훈련시키기(EarlyStopping 추가)
 model.compile(loss='mse', optimizer='adam', 
               metrics=['mae'])
 
@@ -59,12 +60,12 @@ model.fit(x, y, epochs=800, batch_size=1, callbacks=[early_stoppping])# early_st
 # verbose=3 => epoch만 보여줌 
 
 
-# 6. 모델 평가예측
+# 5. 모델 평가예측
 loss, mae = model.evaluate(x, y, batch_size=1) # loss는 자동적으로 출력
 print(loss, mae)
 
 
-# 7. 실제 예측값 구하기
+# 6. 실제 예측값 구하기
 x_input = array([[6.5, 7.5, 8.5], [50, 60, 70], [70, 80, 90], [100, 110, 120]]) 
 # loss가 높은데도 2, 3번째 predict값이 정답에 근접할 때가 있다. 반대로 loss가 낮은데 1번째 값만 정답에 가까운 경우가 생긴다. 
 x_input = x_input.reshape(x_input.shape[0], x_input.shape[1], 1)
