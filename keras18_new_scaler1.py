1491dffe55417236c51c8b7fe5fa86ad42f61228
Winter Lee <leekh090163@gmail.com>
2020-01-30 17:17:49 +0900

    About scaler

---
+++ keras18_new_scaler1.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+
+x = np.array(range(1, 21))
+y = np.array(range(1, 21))
+
+x = x.reshape(20, 1)
+
+print(x.shape)
+print(y.shape)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.5) # random_state를 지정하면 항상 같은 값이 나온다.
+
+print("=======================")
+print(x_train)
+print("=======================")
+print(x_test)
+print("=======================")
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+print("=======================")
+print(x_train)
+print("=======================")
+print(x_test)
+print("=======================")
