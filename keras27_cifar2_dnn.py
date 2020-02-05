b161904be96700b258c31a4955391ac1b683f170
Winter Lee <leekh090163@gmail.com>
2020-02-05 18:32:17 +0900

    File added

--- keras27_cifar2_dnn.py
+++ keras27_cifar2_dnn.py
@@ -1,6 +1,6 @@
 from keras.datasets import cifar10
 from keras.models import Sequential 
-from keras.layers import Dense, Flatten, LSTM, Conv2D, MaxPooling2D, Reshape
+from keras.layers import Dense, Flatten, LSTM, Conv2D, MaxPooling2D, Reshape, BatchNormalization, Dropout
 from keras.callbacks import EarlyStopping 
 import numpy as np
 
@@ -28,10 +28,14 @@ print(y_train.shape) # 분류 클래스 개수만큼 생성
 # 3. model 만들기
 model = Sequential()
 model.add(Dense(512, input_shape=(x_train.shape[1],))) # 가능하면 변수로 바꾸자:)
-model.add(Dense(256, activation='selu'))
-model.add(Dense(256, activation='selu'))
-model.add(Dense(256, activation='selu'))
-model.add(Dense(256, activation='selu'))
+model.add(Dense(256, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))# activation은 add()가 아닌 Dense()에 포함되어 있다!
 
 model.summary()
@@ -42,7 +46,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 early_stopping = EarlyStopping(monitor='loss', patience=20)
 hist = model.fit(x_train, y_train, validation_split=0.2, 
-          epochs=100, batch_size=30,
+          epochs=50, batch_size=100,
           callbacks=[early_stopping])
 
 acc = model.evaluate(x_test, y_test)
@@ -62,3 +66,6 @@ plt.legend(['train loss', 'test loss', 'train loss', 'test acc'])
 plt.show()
 
 # [1.5487799673080445, 0.4557]
+# [2.6441680992126466, 0.4655] => batch_size 조정(30 => 100), epochs 조정(100=>50)/Overfitting 발생
+# [2.248015411376953, 0.4718] => hidden layer 2개 추가(node 256), BatchNormalizaion, Dropout/Overfitting 발생
+# []
